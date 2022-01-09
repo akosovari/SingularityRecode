34b76264471b97f8d46cbb3d2232b7f9a37193f3
AkosVR <akosvr@gmail.com>
2022-01-09 11:48:31 +0100

    Singularity-24.03-HOTFIX Fixed boat ice false positive

--- src/main/java/theforgtn/checks/movement/Vehicle.java
+++ src/main/java/theforgtn/checks/movement/Vehicle.java
@@ -24,44 +24,46 @@ public class Vehicle extends ReactWith {
     public void onMove(PlayerMoveEvent event) {
         org.bukkit.entity.Player player = event.getPlayer();
         PlayerData data = Main.getInstance().getDataManager().getDataPlayer(player);
-        if (!enabled || !Main.getInstance().enabled || !player.isInsideVehicle() || System.currentTimeMillis() - data.lastOnFeet < 1000 || data.IcePosition) { return; }
+        if (!enabled || !Main.getInstance().enabled || !player.isInsideVehicle() || System.currentTimeMillis() - data.lastOnFeet < 1000 || data.IcePosition) {
+            return;
+        }
         // Anarchy mode
         if (ConfigFile.anarchy_mode_enabled) {
-            if(Main.getInstance().tps_protection) {
+            if (Main.getInstance().tps_protection) {
                 //Boat
                 if (player.getVehicle() instanceof Boat) {
                     //Speed
                     if (data.deltaXZ > 2) {
-                        flag(player,0);
-                        SetBack(player,1);
+                        flag(player, 0);
+                        SetBack(player, 1);
                     }
                     //Fly
                     if (abs(abs(player.getLocation().getBlockX()) - Math.abs(data.USP_X)) > 5 || abs(abs(player.getLocation().getBlockY()) - Math.abs(data.USP_Y)) > 1.5 || abs(abs(player.getLocation().getBlockZ()) - Math.abs(data.USP_Z)) > 5) {
                         if (data.deltaY >= 0) {
-                                flag(player,0);
-                                SetBack(player,1);
+                            flag(player, 0);
+                            SetBack(player, 1);
                         }
                     }
                 }
                 //Horse
                 if (player.getVehicle() instanceof Horse) {
                     if (data.deltaXZ > 1) {
-                        flag(player,0);
-                        SetBack(player,1);
+                        flag(player, 0);
+                        SetBack(player, 1);
                     }
                 }
                 //Pig
                 if (player.getVehicle() instanceof Pig) {
                     if (data.deltaXZ > 1) {
-                        flag(player,0);
-                        SetBack(player,1);
+                        flag(player, 0);
+                        SetBack(player, 1);
                     }
                 }
                 //Strider
                 if (player.getVehicle() instanceof Strider) {
                     if (data.deltaXZ > 1) {
-                        flag(player,0);
-                        SetBack(player,1);
+                        flag(player, 0);
+                        SetBack(player, 1);
                     }
                 }
             }
@@ -70,38 +72,44 @@ public class Vehicle extends ReactWith {
             if (player.getVehicle() instanceof Boat) {
                 if (abs(abs(player.getLocation().getBlockX()) - Math.abs(data.USP_X)) > 5 || abs(abs(player.getLocation().getBlockY()) - Math.abs(data.USP_Y)) > 1.5 || abs(abs(player.getLocation().getBlockZ()) - Math.abs(data.USP_Z)) > 5) {
                     if (data.deltaY >= 0) {
-                            flag(player,0);
-                            SetBack(player,1);
+                        flag(player, 0);
+                        SetBack(player, 1);
                     }
                 }
-                if (data.deltaXZ > 0.4) {
-                    flag(player,0);
-                    SetBack(player,1);
-
+                if (data.IcePosition) {
+                    if (data.deltaXZ > 0.4) {
+                        flag(player, 0);
+                        SetBack(player, 1);
+                    } else {
+                        if (data.deltaXZ > 0.8) {
+                            flag(player, 0);
+                            SetBack(player, 1);
+                        }
+                    }
                 }
-            }
-            //Horse
-            if (player.getVehicle() instanceof Horse) {
-                if (data.deltaXZ > 1) {
-                    flag(player,0);
-                    SetBack(player,1);
+                //Horse
+                if (player.getVehicle() instanceof Horse) {
+                    if (data.deltaXZ > 1) {
+                        flag(player, 0);
+                        SetBack(player, 1);
 
+                    }
                 }
-            }
-            //Pig
-            if (player.getVehicle() instanceof Pig) {
-                if (data.deltaXZ > 1) {
-                    flag(player,0);
-                    SetBack(player,1);
+                //Pig
+                if (player.getVehicle() instanceof Pig) {
+                    if (data.deltaXZ > 1) {
+                        flag(player, 0);
+                        SetBack(player, 1);
 
+                    }
                 }
-            }
-            //Strider
-            if (player.getVehicle() instanceof Strider) {
-                if (data.deltaXZ > 1) {
-                    flag(player,0);
-                    SetBack(player,1);
+                //Strider
+                if (player.getVehicle() instanceof Strider) {
+                    if (data.deltaXZ > 1) {
+                        flag(player, 0);
+                        SetBack(player, 1);
 
+                    }
                 }
             }
         }
